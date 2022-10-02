040dc080f5427466ecaf1d4e621ad7e9c6768885
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-02 21:23:29 +0300

    Loaded up all the textures

--- minecraft-clone/src/images/textures.js
+++ minecraft-clone/src/images/textures.js
@@ -8,4 +8,8 @@ import {
         woodImg
         } from './images';
 
-const dirtTexture = 
+const dirtTexture = new TextureLoader(dirtImg);
+const logTexture = new TextureLoader(logImg);
+const grassTexture = new TextureLoader(grassImg);
+const glassTexture = new TextureLoader(glassImg);
+const woodTexture = new TextureLoader(woodImg);
