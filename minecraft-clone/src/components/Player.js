b54e054f1f123491add78878e815b5604e0931a8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-03 22:07:13 +0300

    Implemented a blank useEffect hook

--- minecraft-clone/src/components/Player.js
+++ minecraft-clone/src/components/Player.js
@@ -1,6 +1,6 @@
 import { useSphere } from '@react-three/cannon';
 import { useFrame, useThree } from '@react-three/fiber';
-import React, { useRef } from 'react';
+import React, { useRef, useEffect } from 'react';
 import { Vector3 } from 'three';
 
 
@@ -13,6 +13,11 @@ const Player = () => {
     }));
 
     const pos = useRef([ 0, 0, 0]);
+
+    useEffect(() => {
+        
+    }, []);
+    
 
     useFrame(() => {
         camera.position.copy(new Vector3(pos.current[0], pos.current[1], pos.current[1]));
