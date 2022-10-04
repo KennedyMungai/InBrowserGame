412c07af0be694bf9151a7b6cf3c8b7bec11fd8d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-04 21:04:04 +0300

    Added the centered css class

--- minecraft-clone/src/App.js
+++ minecraft-clone/src/App.js
@@ -19,7 +19,7 @@ function App() {
           <Ground />
         </Physics>
       </Canvas>
-      <div className='absolute cursor'>+</div>
+      <div className='absolute centered cursor'>+</div>
     </>
   );
 }
