31952764de0d6b20a1c6e65c036f3944fdca51dc
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-04 20:57:09 +0300

    Added the basic setup code

---
+++ minecraft-clone/src/components/FPV.js
@@ -0,0 +1,13 @@
+import { useThree } from '@react-three/fiber';
+import React from 'react';
+
+
+const FPV = () => {
+    const { camera } = useThree();
+
+    return (
+        <div>FPV</div>
+    )
+}
+
+export default FPV
