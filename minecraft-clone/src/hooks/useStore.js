9f60105214ef4a06f4e34918c6116d57b1c5589f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-06 21:22:20 +0300

    The document got formatted

--- minecraft-clone/src/hooks/useStore.js
+++ minecraft-clone/src/hooks/useStore.js
@@ -2,10 +2,11 @@ import create from 'zustand';
 import React from 'react';
 
 
-const useStore = () => {
-  return (
-    <div>useStore</div>
-  )
+const useStore = () =>
+{
+    return (
+        <div>useStore</div>
+    )
 }
 
 export default useStore
