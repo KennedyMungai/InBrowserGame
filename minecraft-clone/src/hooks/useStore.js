400c64d54a7649821f2d446a9bd065cdc66d5f51
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-06 21:34:32 +0300

    Added some default values for the cube for debugging purposes

--- minecraft-clone/src/hooks/useStore.js
+++ minecraft-clone/src/hooks/useStore.js
@@ -5,7 +5,11 @@ import { nanoid } from 'nanoid';
 export const useStore = create((set) => (
     {
         texture: 'dirt',
-        cubes: [],
+        cubes: [{
+            key: nanoid(),
+            pos: [1, 1, 1],
+            texture: 'dirt'
+        }],
         addCube: (x, y, z) =>
         {
             set((prev) => ({
