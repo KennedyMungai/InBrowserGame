57d97b50ff61457dbde05698282f046047ca812d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-16 12:46:50 +0300

    Changed up the addition and the subtraction operations

--- minecraft-clone/src/components/Cube.js
+++ minecraft-clone/src/components/Cube.js
@@ -26,23 +26,23 @@ const Cube = ({ position, texture }) =>
 
                 if (clickedFace === 0)
                 {
-                    addCube(x - 1, y, z);
+                    addCube(x + 1, y, z);
                 } else if (clickedFace === 1)
                 {
-                    addCube(x + 1, y, z);
+                    addCube(x - 1, y, z);
 
                 } else if (clickedFace === 2)
                 {
-                    addCube(x, y - 1, z);
+                    addCube(x, y + 1, z);
                 } else if (clickedFace === 3)
                 {
-                    addCube(x, y + 1, z);
+                    addCube(x, y - 1, z);
                 } else if (clickedFace === 4)
                 {
-                    addCube(x, y, z - 1);
+                    addCube(x, y, z + 1);
                 } else if (clickedFace === 5)
                 {
-                    addCube(x, y, z + 1);
+                    addCube(x, y, z - 1);
                 }
             }}
         >
