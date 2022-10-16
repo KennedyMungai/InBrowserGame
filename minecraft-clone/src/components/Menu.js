f855bbdb117751130c3c842785985e5048729abd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-16 15:34:29 +0300

    Added buttons that call the saveWorld and resetWord functions

--- minecraft-clone/src/components/Menu.js
+++ minecraft-clone/src/components/Menu.js
@@ -7,7 +7,10 @@ const Menu = () =>
     const [saveWorld, resetWorld] = useStore((state) => [state.saveWorld, state.resetWorld]);
 
     return (
-        <div>Menu</div>
+        <div className='menu absolute'>
+            <button onClick={() => saveWorld()}>Save</button>
+            <button onClick={() => resetWorld()}>Reset</button>
+        </div>
     )
 }
 
