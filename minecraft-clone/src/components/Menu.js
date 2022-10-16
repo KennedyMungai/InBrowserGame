70e1c3c10d2c6e51701d6c123faee00e207a9666
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-16 15:31:28 +0300

    Added references for the saveWorld and resetWorld functions

--- minecraft-clone/src/components/Menu.js
+++ minecraft-clone/src/components/Menu.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { useStore } from '../hooks/useStore';
 
 
 const Menu = () =>
 {
+    const [saveWorld, resetWorld] = useStore((state) => [state.saveWorld, state.resetWorld]);
+
     return (
         <div>Menu</div>
     )
