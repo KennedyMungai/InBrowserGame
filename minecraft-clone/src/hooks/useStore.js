8cafefcd575dbf2869fc0f6d0f72d0cd2690d93d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-16 15:22:25 +0300

    Created the setLocalStorage helper function

--- minecraft-clone/src/hooks/useStore.js
+++ minecraft-clone/src/hooks/useStore.js
@@ -3,6 +3,7 @@ import { nanoid } from 'nanoid';
 
 
 const getLocalStorage = (key) => JSON.parse(window.localStorage.getItem(key));
+const setLocalStorage = (key, value) => window.localStorage.setItem(key, JSON.stringify(value));
 
 
 export const useStore = create((set) => (
