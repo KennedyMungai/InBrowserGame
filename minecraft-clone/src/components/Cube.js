8766f58b7301d44e5544035238be478c14bb31e1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2022-10-16 12:48:01 +0300

    Added return statements to all the available cases

--- minecraft-clone/src/components/Cube.js
+++ minecraft-clone/src/components/Cube.js
@@ -27,22 +27,28 @@ const Cube = ({ position, texture }) =>
                 if (clickedFace === 0)
                 {
                     addCube(x + 1, y, z);
+                    return;
                 } else if (clickedFace === 1)
                 {
                     addCube(x - 1, y, z);
+                    return;
 
                 } else if (clickedFace === 2)
                 {
                     addCube(x, y + 1, z);
+                    return;
                 } else if (clickedFace === 3)
                 {
                     addCube(x, y - 1, z);
+                    return;
                 } else if (clickedFace === 4)
                 {
                     addCube(x, y, z + 1);
+                    return;
                 } else if (clickedFace === 5)
                 {
                     addCube(x, y, z - 1);
+                    return;
                 }
             }}
         >
